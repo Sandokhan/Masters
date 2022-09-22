fd3542c3f46a830d9b78086bed6f339b19f14b62
sandokhan <rhaydrick@gmail.com>
2022-09-22 22:31:17 +0100

    add gropy_by examples.

--- 1S/VPD/groupsummary.r
+++ 1S/VPD/groupsummary.r
@@ -15,11 +15,24 @@ gapminder %>%
 # Filter for 1957 then summarize the median life expectancy and the maximum GDP per capita
 gapminder %>%
   filter(year == 1957) %>%
-  summarize(medianLifeExp = median(lifeExp), 
+  summarize(medianLifeExp = median(lifeExp),
             maxGdpPercap = max(gdpPercap))
 
-
+# Find median life expectancy and maximum GDP per capita in each year
 gapminder %>%
   group_by(year) %>%
-  summarize(medianLifeExp = median(lifeExp), 
+  summarize(medianLifeExp = median(lifeExp),
             maxGdpPercap = max(gdpPercap))
+
+# Find median life expectancy and maximum GDP per capita in each continent in 1957
+gapminder %>%
+    filter(year == 1957) %>%
+    group_by(continent) %>%
+    summarize(medianLifeExp = median(lifeExp),
+    maxGdpPercap = max(gdpPercap))
+
+# Find median life expectancy and maximum GDP per capita in each continent/year combination
+gapminder %>%
+    group_by(continent, year) %>%
+    summarize(medianLifeExp = median(lifeExp),
+    maxGdpPercap = max(gdpPercap))
