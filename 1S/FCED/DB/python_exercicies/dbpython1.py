46921afc2ca5e06786ac732b771dd1d352310661
sandokhan <rhaydrick@gmail.com>
2022-09-24 10:18:13 +0100

    Add function to connect to database

---
+++ 1S/FCED/DB/python_exercicies/dbpython1.py
@@ -0,0 +1,16 @@
+import psycopg2 
+
+con = psycopg2.connect(
+  database="fced_rhaydrick_tavares",             # your database is the same as your username
+  user="fced_rhaydrick_tavares",                 # your username
+  password="Rhaydrick@0358",             # your password
+  host="dbm.fe.up.pt",             # the database host
+  options='-c search_path=employee',
+  port='5433'  # use the schema you want to connect to
+)
+
+id = int(input('Employee ID: '))
+cur = con.cursor()
+cur.execute(f'SELECT * FROM employee WHERE id = {id}')
+employee = cur.fetchone()
+print(employee)
