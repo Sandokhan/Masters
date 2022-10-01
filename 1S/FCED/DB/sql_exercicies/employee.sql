50d9615c2b3d2bbff77660522469b16ad473ef4f
sandokhan <rhaydrick@gmail.com>
2022-10-01 20:52:59 +0100

    Add new questions and SQL solutions.

--- 1S/FCED/DB/sql_exercicies/employee.sql
+++ 1S/FCED/DB/sql_exercicies/employee.sql
@@ -34,14 +34,13 @@ JOIN department d ON d.id = e.id_dep;
 
 SELECT 
     d.name AS department_name,
-    e.name AS employee_name
+    e.name AS director_name
 FROM employee e
-JOIN department d ON d.id = e.id_dep
-WHERE e.id_sup IS NULL;
+JOIN department d ON d.id_dir = e.id;
 
 /* Result
 
- department_name | employee_name 
+ department_name | director_name 
 -----------------+---------------
  Logistics       | John
  Transports      | Theresa
@@ -65,30 +64,126 @@ JOIN department d ON p.id_dep = d.id;
  ZPTO         | Informatics */
 
 -- 4. What projects are controlled by department number 1. (project_name)
+
+SELECT 
+    name
+FROM project
+WHERE id_dep = 1;
+
+/* Result
+ name 
+------
+ XPTO */
+
 -- 5. What projects are controlled by the Transports department? (project_name)
+
+SELECT 
+    p.name
+FROM project p
+JOIN department d ON p.id_dep = d.id
+WHERE d.name = 'Transports';
+
+/* Result
+ name 
+------
+ YPTO */
+
 -- 6. List all employees working on project XPTO, and how many hours each one of them works in that project. 
 -- (employee_name, hours)
+
+SELECT
+    e.name employee_name,
+    SUM(hours)
+ FROM employee e
+JOIN works w ON e.id = w.id_emp
+JOIN project p  USING(id_dep)
+WHERE p.name = 'XPTO'
+GROUP BY 1;
+
+/* Result
+ employee_name | sum 
+---------------+-----
+ John          |  19
+ Mike          |  10 */
+
 -- 7. List all employees working on project XPTO,
 -- and how many hours each one of them works in that project. 
+-- Order the list from the employee that works more hours to the one that works less. (employee_name, hours)
 SELECT
-    e.name,
-    SUM(hours)
+    e.name employee_name,
+    SUM(hours) hours
 FROM employee e
 JOIN works w ON e.id = w.id_emp
 JOIN project p  USING(id_dep)
 WHERE p.name = 'XPTO'
 GROUP BY 1
--- Order the list from the employee that works more hours to the one that works less. (employee_name, hours)
+ORDER BY SUM(hours) DESC;
+
+/* Result
+ employee_name | hours 
+---------------+-------
+ John          |    19
+ Mike          |    10 */
+
 -- 8. How many employees work on each project? (project_name, number)
+SELECT 
+    p.name project_name,
+    COUNT(w.id_emp) number
+FROM works w
+JOIN project p ON w.id_pro = p.id
+GROUP BY 1;
+
+/* Result
+ project_name | number 
+--------------+--------
+ YPTO         |     10
+ ZPTO         |      4
+ XPTO         |      9 */
+
 -- 9. How many hours are spent weekly on each project? (project_name, hours)
+
+SELECT 
+    p.name project_name,
+    SUM(w.hours) hours
+FROM works w
+JOIN project p ON w.id_pro = p.id
+GROUP BY 1;
+
+/* Result
+ project_name | hours 
+--------------+-------
+ YPTO         |    42
+ ZPTO         |    12
+ XPTO         |    33 */
+
+
 -- 10. How many hours does each employee spend on projects every week? (employee_name, hours)
 
 SELECT
-    e.name,
-    SUM(hours)
+    e.name employee_name,
+    SUM(hours) hours
 FROM employee e
 JOIN works w ON e.id = w.id_emp
 GROUP BY e.name;
+
+/* Result
+ employee_name | hours 
+---------------+-------
+ Thomas        |     4
+ Mary          |     3
+ Beatrice      |     2
+ Mike          |    10
+ Kate          |     6
+ Megan         |     6
+ Theresa       |     2
+ Carrie        |     9
+ Charles       |    10
+ John          |    19
+ James         |     5
+ Barry         |     1
+ Sarah         |     5
+ Emily         |     3
+ Harry         |     2 */
 
 -- 11. List all employees and their superiors. (employee_name, superior_name)
 
